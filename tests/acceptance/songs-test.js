9635a4a4c9f9c3399868a96e8486193c48bcf2a9
natalia_viskovatykh@epam.com <natalia_viskovatykh@epam.com>
2024-08-24 18:59:06 +0300

    Update tests

--- tests/acceptance/songs-test.js
+++ tests/acceptance/songs-test.js
@@ -9,9 +9,11 @@ module('Acceptance | songs', function (hooks) {
 
   test('Search songs', async function (assert) {
     let band = this.server.create('band', { name: 'Crooked Vultures' });
+    let idBand = band.attrs.id;
+
+    this.server.create('song', { title: 'Crocodiles', rating: 1, band });
     this.server.create('song', { title: 'Elephants', rating: 2, band });
     this.server.create('song', { title: 'Tortillas', rating: 5, band });
-    this.server.create('song', { title: 'Crocodiles', rating: 1, band });
     this.server.create('song', { title: 'Shark', rating: 3, band });
 
     await visit('/');
@@ -23,18 +25,15 @@ module('Acceptance | songs', function (hooks) {
       );
 
     await click('[data-test-rr="band-link"]');
-    await visit('/bands/crooked-vultures/songs');
+    await visit(`/bands/${idBand}/songs`);
     await click('[data-test-rr="songs-nav-item"]');
-
-    await visit(`/bands/${band.id}/songs`);
-    settled();
-
     assert
       .dom('[data-test-rr="song-list-item"]:first-child')
-      .hasText('Crocodiles');
+      .includesText('Crocodiles');
+
     assert
       .dom('[data-test-rr="song-list-item"]:last-child')
-      .hasText('Tortillas');
+      .includesText('Tortillas');
 
     await fillIn('[data-test-rr="search-box"]', 'el');
     assert
@@ -44,6 +43,7 @@ module('Acceptance | songs', function (hooks) {
 
   test('Sort songs in various ways', async function (assert) {
     let band = this.server.create('band', { name: 'Crooked Vultures' });
+    let idBand = band.attrs.id;
     this.server.create('song', { title: 'Elephants', rating: 2, band });
     this.server.create('song', { title: 'Tortillas', rating: 5, band });
     this.server.create('song', { title: 'Crocodiles', rating: 1, band });
@@ -58,119 +58,27 @@ module('Acceptance | songs', function (hooks) {
       );
 
     await click('[data-test-rr="band-link"]');
-    await visit('/bands/crooked-vultures/songs');
+    await visit(`/bands/${idBand}/songs`);
     await click('[data-test-rr="songs-nav-item"]');
 
-    await visit(`/bands/${band.id}/songs`);
     await click('[data-test-rr=sort-by-title-desc]');
     assert
       .dom('[data-test-rr="song-list-item"]:first-child')
-      .hasText('Tortillas');
+      .includesText('Tortillas');
     assert
       .dom('[data-test-rr="song-list-item"]:last-child')
-      .hasText('Crocodiles');
+      .includesText('Crocodiles');
 
     await click('[data-test-rr=sort-by-rating-asc]');
     assert
       .dom('[data-test-rr=song-list-item]:first-child')
-      .hasText('Crocodiles', 'The first song is the lowest rated');
+      .includesText('Crocodiles', 'The first song is the lowest rated');
     assert
       .dom('[data-test-rr=song-list-item]:last-child')
-      .hasText('Tortillas', 'The last song is the highest rated');
+      .includesText('Tortillas', 'The last song is the highest rated');
     assert.ok(
       currentURL().includes('s=rating'),
       'The sort query param appears in the URL with the correct value',
     );
-
-    //   // let band = this.server.create('band', { name: 'Crooked Vultures' });
-    //   // await visit('/');
-    //   // await createBand('Caspian');
-    //   // await waitFor('[data-test-rr="no-songs-text"]');
-
-    //   // let band = this.server.create('band', { name: 'Crooked Vultures' });
-    //   this.server.create('song', {
-    //     title: 'Mind Eraser',
-    //     rating: 2,
-    //     band,
-    //   });
-    //   this.server.create('song', { title: 'Elephants', rating: 4, band });
-    //   this.server.create('song', {
-    //     title: 'Spinning in Daffodils',
-    //     rating: 5,
-    //     band,
-    //   });
-    //   this.server.create('song', { title: 'New Fang', rating: 3, band });
-
-    //   // await visit('/bands/them-crooked-vultures/songs');
-    //   // await waitFor('[data-test-rr=band-link]');
-    //   await visit('/');
-    //   await createBand('Crooked Vultures');
-    //   await waitFor('[data-test-rr="no-songs-text"]');
-    //   await click('[data-test-rr="band-link"]');
-    //   await settled();  // Ждем завершения всех асинхронных процессов
-
-    //   assert
-    //     .dom('[data-test-rr="song-list-item"]:first-child')
-    //     .hasText(
-    //       'Elephants',
-    //       'The first song is the one that comes first in the alphabet',
-    //     );
-    //   assert
-    //     .dom('[data-test-rr="song-list-item"]:last-child')
-    //     .hasText(
-    //       'Spinning In Daffodils',
-    //       'The last song is the one that comes last in the alphabet',
-    //     );
-
-    //   // await waitFor('[data-test-rr=sort-by-title-desc]');
-    //   await click('[data-test-rr=sort-by-title-desc]');
-    //   // await settled();  // Ждем завершения всех асинхронных процессов
-
-    //   assert
-    //     .dom('[data-test-rr=song-list-item]:first-child')
-    //     .hasText(
-    //       'Spinning In Daffodils',
-    //       'The first song is the one that comes last in the alphabet',
-    //     );
-    //   assert
-    //     .dom('[data-test-rr=song-list-item]:last-child')
-    //     .hasText(
-    //       'Elephants',
-    //       'The last song is the one that comes first in the alphabet',
-    //     );
-    //   assert.ok(
-    //     currentURL().includes('s=-title'),
-    //     'The sort query param appears in the URL with the correct value',
-    //   );
-
-    //   // await waitFor('[data-test-rr=sort-by-rating-asc]');
-    //   await click('[data-test-rr=sort-by-rating-asc]');
-    //   // await settled();  // Ждем завершения всех асинхронных процессов
-
-    //   assert
-    //     .dom('[data-test-rr=song-list-item]:first-child')
-    //     .hasText('Mind Eraser', 'The first song is the lowest rated');
-    //   assert
-    //     .dom('[data-test-rr=song-list-item]:last-child')
-    //     .hasText('Spinning In Daffodils', 'The last song is the highest rated');
-    //   assert.ok(
-    //     currentURL().includes('s=rating'),
-    //     'The sort query param appears in the URL with the correct value',
-    //   );
-
-    //   // await waitFor('[data-test-rr=sort-by-rating-desc]');
-    //   await click('[data-test-rr=sort-by-rating-desc]');
-    //   // await settled();  // Ждем завершения всех асинхронных процессов
-
-    //   assert
-    //     .dom('[data-test-rr=song-list-item]:first-child')
-    //     .hasText('Spinning In Daffodils', 'The first song is the highest rated');
-    //   assert
-    //     .dom('[data-test-rr=song-list-item]:last-child')
-    //     .hasText('Mind Eraser', 'The last song is the lowest rated');
-    //   assert.ok(
-    //     currentURL().includes('s=-rating'),
-    //     'The sort query param appears in the URL with the correct value',
-    //   );
   });
 });
--- tests/helpers/custom-helpers.js
+++ tests/helpers/custom-helpers.js
@@ -5,9 +5,3 @@ export async function createBand(name) {
   await fillIn('[data-test-rr="new-band-name"]', name);
   return click('[data-test-rr="save-band-button"]');
 }
-
-export async function createSong(name) {
-  await click('[data-test-rr="new-band-button"]');
-  await fillIn('[data-test-rr="new-band-name"]', name);
-  return click('[data-test-rr="save-band-button"]');
-}
